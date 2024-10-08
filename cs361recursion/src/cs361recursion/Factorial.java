15e05c697de9561ec2edbd35eecaf979715a2f44
Myron Zun Hao Weng <wengmyron@gmail.com>
2024-10-07 23:42:53 -0400

    factorial.java formatting

--- cs361recursion/src/cs361recursion/Factorial.java
+++ cs361recursion/src/cs361recursion/Factorial.java
@@ -11,20 +11,23 @@ public class Factorial {
 
 	/**
 	 * factorial function
-	 * @param n  
+	 * 
+	 * @param n
 	 * @return the factorial of n
 	 * @throws IllegalArgumentException if n < 0 or n > 12. Factorial should not
-	 * accept a negative number as an argument. For n > 12, factorial(n) is not an int anymore. 
+	 *                                  accept a negative number as an argument. For
+	 *                                  n > 12, factorial(n) is not an int anymore.
 	 */
 	public static int factorial(int n) {
 		if (n < 0 || n > 12) {
 			throw new IllegalArgumentException("n should be between 0 and 12");
 		} else {
-		if (n==0) {
-			return 1;
-		} else {
-			return n*factorial(n-1);
-		}}
+			if (n == 0) {
+				return 1;
+			} else {
+				return n * factorial(n - 1);
+			}
+		}
 	}
 
 	/**
